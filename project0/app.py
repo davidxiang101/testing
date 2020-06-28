c17d29638531087fc37d7ce57952b72bbabab02b
davidxiang101 <43865832+davidxiang101@users.noreply.github.com>
2020-06-27 20:28:39 -0400

    fixed flask now each page has its own separate url and links work properly

---
+++ project0/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("homepage.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/work")
+def work():
+    return render_template("work.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
